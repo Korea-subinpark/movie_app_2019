e072436d2dc1426ce46ef81e54e5052666320f3f
Korea-subinpark <subinpark.re@gmail.com>
2019-12-01 09:57:33 +0900

    Updates

---
+++ precache-manifest.bc8ff3f7dc0eaabcfca1e009bb0e1a96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36f8fc59e6bea16932485bc4ff37f0f",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "48e4c3addc4260304c84",
+    "url": "/movie_app_2019/static/css/main.5eed4404.chunk.css"
+  },
+  {
+    "revision": "18e17f1c73cef50e947f",
+    "url": "/movie_app_2019/static/js/2.e25caf30.chunk.js"
+  },
+  {
+    "revision": "48e4c3addc4260304c84",
+    "url": "/movie_app_2019/static/js/main.85cbd23c.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
